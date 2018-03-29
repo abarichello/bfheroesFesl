557a48bd2282e33c85123881099ada122e55aa42
Synaxis <01xtor@gmail.com>
2018-03-29 16:04:42 -0300

    up

--- inter/network/codec/decoder.go
+++ inter/network/codec/decoder.go
@@ -1,7 +1,7 @@
 package codec
 
-func DecodeFESL(data []byte) map[string]string {
-	out := map[string]string{}
+func DecodeFESL(data []byte) Fields {
+	out := Fields{}
 	key := []byte{}
 	pos := 0
 	for i, c := range data {
